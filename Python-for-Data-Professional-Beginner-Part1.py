297296988794af0d608e1de80c005cf610642913
Salma Fauziah <salmafauziah481@gmail.com>
2023-03-18 20:28:18 +0700

    Update Python-for-Data-Professional-Beginner-Part1.py

--- Python-for-Data-Professional-Beginner-Part1.py
+++ Python-for-Data-Professional-Beginner-Part1.py
@@ -1,4 +1,38 @@
-#Program pertama: "Hello World"#
+# Program pertama: "Hello World"
 print("Hello World!")
 
-#
+# Program Pertamaku
+# Sekarang aku diberikan tugas dari Senja untuk menampilkan kalimat “Halo Dunia” dan “Riset Bahasa Python” menggunakan fungsi print()#
+print("Halo Dunia")
+print("Riset Bahasa Python")
+
+# Struktur Program Python - Part 1
+# Statement
+print("Belajar Python Menyenangkan") 
+print("Halo Dunia")
+print("Hello World!")
+
+# Variables & Literals
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+
+# Operators
+print(bilangan1 + bilangan2)
+
+# Tugas Praktek
+# Deklarasi variable bilangan1 dengan 20, dan bilangan2 dengan 10 dan tampilkan hasil pengurangan bilangan1 & bilangan 2.
+bilangan1 = 20
+bilangan2 = 10
+print(bilangan1 - bilangan2)
+
+# Tugas Praktek
+# Tugas:
+# Aku diminta menghitung harga_setelah_potongan dan harga_final. harga_final diperoleh dengan mengalikan harga_setelah_potongan dengan angka 1.1 karena PPN sebesar 10% (100% + 10% = 110% atau 1.1)
+# Aku menggunakan variabel harga_asli dengan nilai 20000 dan variabel potongan dengan nilai 2000
+harga_asli = 20000
+potongan = 2000
+harga_setelah_potongan = harga_asli - potongan
+harga_final = harga_setelah_potongan * 1.1
+print(harga_final)
+
