c7621a4463294b2146d9935723f56dec5d0214d8
Salma Fauziah <salmafauziah481@gmail.com>
2023-03-28 10:15:29 +0700

    Update and rename Part-1-Select-SQL-Statement.py to Part-1-Select-SQL-Statement.sql

---
+++ 2 - Fundamental SQL/Part-1-Select-SQL-Statement.sql
@@ -0,0 +1,21 @@
+##############################
+# Chapter 1 : Penggunaan Perintah SELECT… FROM #
+# Mengambil Seluruh Kolom dalam suatu Tabel #
+##############################
+
+SELECT * FROM ms_produk;
+
+##############################
+# Chapter 1 : Penggunaan Perintah SELECT… FROM #
+# Mengambil Satu Kolom dari Tabel #
+##############################
+
+SELECT nama_produk FROM ms_produk;
+
+##############################
+# Chapter 1 : Penggunaan Perintah SELECT… FROM #
+# Mengambil Lebih dari Satu Kolom dari Tabel #
+##############################
+
+SELECT nama_produk, harga FROM ms_produk;
+
