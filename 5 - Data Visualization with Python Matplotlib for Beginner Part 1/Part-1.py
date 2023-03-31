efe036c6d51638a6c456e2224219e1c867c71a75
Salma Fauziah <salmafauziah481@gmail.com>
2023-03-31 11:08:52 +0700

    Create Part-1.py

---
+++ 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-1.py
@@ -0,0 +1,299 @@
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Pengenalan Dataset
+#####
+
+import pandas as pd
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+print('Ukuran dataset : %d baris dan %d kolom\n' % dataset.shape)
+print('Lima data teratas:')
+print(dataset.head())
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
+#####
+# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# #####
+
+
+
