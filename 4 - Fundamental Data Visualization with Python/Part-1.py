98b9c6c6b16b4e0356a41ee41f647af8d040e9ef
Salma Fauziah <salmafauziah481@gmail.com>
2023-03-31 10:50:34 +0700

    Update Part-1.py

--- 4 - Fundamental Data Visualization with Python/Part-1.py
+++ 4 - Fundamental Data Visualization with Python/Part-1.py
@@ -4,31 +4,29 @@
 # https://academy.dqlab.id/main/livecode/177/344/2304?pr=0
 
 import pandas as pd
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.style.use("seaborn")
 
-tabel = (pd.read_csv("https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/usia_karyawan.csv")
-.sort_values("Kelompok Usia", ascending=False)
-.set_index("Kelompok Usia")
-)
+tabel = (pd.read_csv("https://storage.googleapis.com/dqlab-dataset/usia_karyawan.csv")
+		 .sort_values("Kelompok Usia", ascending=False)
+		 .set_index("Kelompok Usia")
+		)
 
 tabel["Laki-laki"] = -tabel["Laki-laki"]
 
 fig, axes = plt.subplots(1, 2, figsize=(14, 6))
 fig.suptitle("Perbandingan Jumlah Karyawan Laki-laki dan Perempuan\nBerdasarkan Kelompok Usia",
-x=0., y=1.1, fontsize=24, fontweight='bold', ha="left")
+x=0., y=1.0, fontsize=24, fontweight='bold', ha="left")
 
 tabel["Laki-laki"].plot(kind="barh", ax=axes[0], color="tab:blue", xlim=[-550, 0])
 tabel["Perempuan"].plot(kind="barh", ax=axes[1], color="tab:orange", fontsize=14, xlim=[0, 550])
 
 axes[0].set_ylabel("")
-axes[0].set_xticklabels([""])
-axes[0].set_yticklabels("")
+axes[0].tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
 axes[0].legend(["Laki-laki"], fontsize=14, bbox_to_anchor=(0., 0., 1., .05))
 axes[0].set_frame_on(False)
+
 axes[1].set_ylabel("")
-axes[1].set_xticklabels([""])
+axes[1].tick_params(left=False, bottom=False,labelbottom=False)
 axes[1].legend(["Perempuan"], fontsize=14, bbox_to_anchor=(0., 0., 0.3, .05))
 axes[1].set_frame_on(False)
 
@@ -38,5 +36,5 @@ for i, m, w in zip(range(tabel.shape[0]), list(tabel['Laki-laki']), list(tabel['
   axes[1].annotate(str(abs(w)), (w-20, i), xytext=(w-20, i),
   color="w", va="center", ha="center", fontsize=14)
   
-plt.tight_layout()
+plt.tight_layout(rect=(0,0,1,0.88))
 plt.show()
