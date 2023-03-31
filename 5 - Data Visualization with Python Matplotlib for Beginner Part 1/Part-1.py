38433ba60cb17c76cfa152d1a9f467cada9de647
Salma Fauziah <salmafauziah481@gmail.com>
2023-03-31 11:13:41 +0700

    Update Part-1.py

--- 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-1.py
+++ 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-1.py
@@ -11,21 +11,47 @@ print(dataset.head())
 
 #####
 # Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
+# Penambahan Kolom Order Month pada Dataset
+#####
 
+import pandas as pd
+import datetime
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+
+dataset['order_month'] = dataset['order_date'].apply(lambda x:datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+print(dataset.head())
 
 #####
 # Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
+# Penambahan Kolom GMV pada Dataset
+#####
 
+import pandas as pd
+import datetime
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+print('Ukuran dataset: %d baris dan %d kolom\n' % dataset.shape)
+print('Lima data teratas:')
+print(dataset.head())
 
 #####
 # Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
+# Membuat Data Agregat
+#####
 
+import pandas as pd
+import datetime
 
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+print(monthly_amount)
 
 #####
 # Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
