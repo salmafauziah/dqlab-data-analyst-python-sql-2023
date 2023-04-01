86b63ef07690f28f969b736ac79ae2e951e6e00e
Salma Fauziah <salmafauziah481@gmail.com>
2023-04-01 10:38:34 +0700

    Update Part-1.py

--- 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-1.py
+++ 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-1.py
@@ -39,7 +39,7 @@ print('Lima data teratas:')
 print(dataset.head())
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 2 : Plot Pertama dengan Matplotlib
 # Membuat Data Agregat
 #####
 
@@ -54,271 +54,181 @@ monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
 print(monthly_amount)
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 2 : Plot Pertama dengan Matplotlib
+# Plot Pertama: Membuat Line Chart Trend Pertumbuhan GMV
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+monthly_amount = dataset.groupby('order_month')['gmv'].sum().reset_index()
+
+plt.plot(monthly_amount['order_month'], monthly_amount['gmv'])
+plt.show()
+
+#####
+# Chapter 2 : Plot Pertama dengan Matplotlib
+# Cara Alternatif: Fungsi .plot() pada pandas Dataframe
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
+
+#####
+# Chapter 3 : Kustomisasi Grafik - Part 1
+# Mengubah Figure Size
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+plt.figure(figsize=(15,5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.show()
+
+#####
+# Chapter 3 : Kustomisasi Grafik - Part 1
+# Menambahkan Title and Axis Labels
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+
+plt.title('Monthly GMV Year 2019')
+plt.xlabel('Order Month')
+plt.ylabel('Total GMV')
+
+plt.show()
+
+#####
+# Chapter 3 : Kustomisasi Grafik - Part 1
+# Kustomisasi Title and Axis Labels
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot()
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+plt.show()
+
+#####
+# Chapter 3 : Kustomisasi Grafik - Part 1
+# Kustomisasi Line dan Point
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+plt.show()
+
+#####
+# Chapter 3 : Kustomisasi Grafik - Part 1
+# Kustomisasi Grid
+#####
+
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+plt.figure(figsize=(15, 5))
+dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
+
+plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount', fontsize=15)
+
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.show()
+
+#####
+# Chapter 4 : Kustomisasi Grafik - Part 2
+# Kustomisasi Axis Ticks
+#####
+
+
+
+#####
+# Chapter 4 : Kustomisasi Grafik - Part 2
 # #####
 
 
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 4 : Kustomisasi Grafik - Part 2
 # #####
 
 
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 4 : Kustomisasi Grafik - Part 2
 # #####
 
 
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 4 : Kustomisasi Grafik - Part 2
 # #####
 
 
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 4 : Kustomisasi Grafik - Part 2
 # #####
 
 
 
 #####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
-# #####
-
-
-
-#####
-# Chapter 1 : Pengenalan Matplotlib dan Persiapan Dataset
+# Chapter 4 : Kustomisasi Grafik - Part 2
 # #####
 
 
