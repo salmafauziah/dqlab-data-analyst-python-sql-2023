80a779c99b4f13d4d031dc9a1215b5279cd7586d
Salma Fauziah <salmafauziah481@gmail.com>
2023-04-03 09:38:41 +0700

    Update Part-2.py

--- 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-2.py
+++ 5 - Data Visualization with Python Matplotlib for Beginner Part 1/Part-2.py
@@ -35,198 +35,309 @@ plt.show()
 # Kustomisasi Legend
 ####
 
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
 
+dataset.groupby(['order_month','brand'])['gmv'].sum().unstack().plot()
+plt.title('Monthly GMV Year 2019 - Breakdown by Brand',loc='center',pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)',fontsize = 15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
 
-####
-# Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
-# ####
-
-
-
-####
-# Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
-# ####
-
-
-
-####
-# Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
-# ####
-
-
-
-####
-# Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
-# ####
-
-
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='right', bbox_to_anchor=(1.6, 0.5), shadow=True, ncol=2)
+plt.gcf().set_size_inches(12, 5)
+plt.tight_layout()
+plt.show()
 
 ####
 # Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
-# ####
+# Kustomisasi Colormap
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+plt.clf()
+dataset.groupby(['order_month', 'province'])['gmv'].sum().unstack().plot(cmap='Set1')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province', loc='center', pad = 30, fontsize = 20, color = 'blue')
+plt.xlabel('Order Month', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)', fontsize = 15)
+plt.grid(color = 'darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin = 0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.5), shadow=True, ncol=3, title='province',fontsize=9, title_fontsize=11)
+plt.gcf().set_size_inches(10,5)
+plt.tight_layout()
+plt.show()
+
+####
+# Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
+# Membuat Line Chart GMV Breakdown by Top Provinces
+####
+
+import datetime
+import pandas as pd
+
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+# Buat variabel untuk 5 propinsi dengan GMV tertinggi
+top_provinces = (dataset.groupby('province')['gmv']
+				 .sum()
+				 .reset_index()
+				 .sort_values(by='gmv', ascending=False)
+				 .head(5))
+print(top_provinces)
+
+# Buat satu kolom lagi di dataset dengan nama province_top
+dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
+
+# Plot multi-line chartnya
+import matplotlib.pyplot as plt
+dataset.groupby(['order_month', 'province_top'])['gmv'].sum().unstack().plot(marker='.', cmap='plasma')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province', loc='center', pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='upper center', bbox_to_anchor=(1.1, 1), shadow=True, ncol=1)
+plt.gcf().set_size_inches(12, 5)
+plt.tight_layout()
+plt.show()
+
+####
+# Chapter 2 : Membuat Multi-line Chart dan Kustomisasi Lanjutan
+# Membuat Anotasi
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+top_provinces = (dataset.groupby('province')['gmv']
+                        .sum()
+                        .reset_index()
+                        .sort_values(by='gmv',ascending=False)
+                        .head(5))
+dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
+
+dataset.groupby(['order_month','province_top'])['gmv'].sum().unstack().plot(marker='.', cmap='plasma')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province',loc='center',pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)',fontsize = 15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='upper center', bbox_to_anchor=(1.1, 1), shadow=True, ncol=1)
+
+# Anotasi pertama
+plt.annotate('GMV other meningkat pesat', xy=(5, 900000000),
+			 xytext=(4, 1700000000),weight='bold',color='red',
+			 arrowprops=dict(arrowstyle='fancy',
+							 connectionstyle="arc3",
+							 color='red'))
+
+# Anotasi kedua
+plt.annotate('DKI Jakarta mendominasi', xy=(3, 3350000000),
+			xytext=(0, 3700000000), weight='bold', color='red',
+			arrowprops=dict(arrowstyle='->',
+						   connectionstyle='angle',
+						   color='red'))
+
+plt.gcf().set_size_inches(12, 5)
+plt.tight_layout()
+plt.show()
+
+####
+# Chapter 3 : Membuat Pie-Chart dan Bar-Chart
+# Membuat Subset Data
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+print(dataset_dki_q4.head())
+
+####
+# Chapter 3 : Membuat Pie-Chart dan Bar-Chart
+# Membuat Pie Chart
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+gmv_per_city_dki_q4 = dataset_dki_q4.groupby('city')['gmv'].sum().reset_index()
+plt.figure(figsize=(6,6))
+plt.pie(gmv_per_city_dki_q4['gmv'], labels=gmv_per_city_dki_q4['city'], autopct='%1.2f%%')
+plt.title('GMV Contribution Per City - DKI Jakarta in Q4 2019', loc='center', pad=30, fontsize = 15, color='blue')
+plt.show()
+
+####
+# Chapter 3 : Membuat Pie-Chart dan Bar-Chart
+# Membuat Bar Chart
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+plt.clf()
+dataset_dki_q4.groupby('city')['gmv'].sum().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('GMV Per City - DKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
+plt.xlabel('City', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.ylim(ymin=0)
+
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation = 0)
+plt.show()
+
+####
+# Chapter 3 : Membuat Pie-Chart dan Bar-Chart
+# Membuat Multi-Bar Chart
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+dataset_dki_q4.groupby(['city','order_month'])['gmv'].sum().unstack().plot(kind='bar')
+plt.title('GMV Per City, Breakdown by Month\nDKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
+plt.xlabel('Province', fontsize=12)
+plt.ylabel('Total Amount (in Billions)', fontsize=12)
+plt.legend(bbox_to_anchor=(1,1), shadow=True, title='Month')
+plt.ylim(ymin=0)
+
+labels, locations=plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+####
+# Chapter 3 : Membuat Pie-Chart dan Bar-Chart
+# Membuat Stacked Chart
+####
+
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+
+dataset_dki_q4.groupby(['order_month','city'])['gmv'].sum().sort_values(ascending=False).unstack().plot(kind='bar', stacked=True)
+plt.title('GMV Per Month, Breakdown by City\nDKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
+plt.xlabel('Order Month', fontsize=12)
+plt.ylabel('Total Amount (in Billions)', fontsize=12)
+plt.legend(bbox_to_anchor=(1,1), shadow=True, ncol=1, title='City')
+plt.ylim(ymin=0)
+
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
+
+####
+# Chapter 4 : Membuat Histogram dan Scatterplot
+# Membuat Agregat Data Customer
+####
 
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
 
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
 
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
 
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
+
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
+
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
+
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
+
+
 
 
 ####
-# 
+# Chapter 4 : Membuat Histogram dan Scatterplot
 # ####
 
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
-
-
-####
-# 
-# ####
 
 
