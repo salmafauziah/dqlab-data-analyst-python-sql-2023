ee406393e078f1313009dffc5dd61e40813ba2f6
Salma Fauziah <salmafauziah481@gmail.com>
2023-04-05 12:57:37 +0700

    Update Part-2-SQL-DQLab-Store-Overall-Performance.sql

--- 6 - Project Data Analysis for Retail: Sales Performance Report/Part-2-SQL-DQLab-Store-Overall-Performance.sql
+++ 6 - Project Data Analysis for Retail: Sales Performance Report/Part-2-SQL-DQLab-Store-Overall-Performance.sql
@@ -1,5 +1,11 @@
 # Overall Performance by Year
 # Buatlah Query dengan menggunakan SQL untuk mendapatkan total penjualan (sales) dan jumlah order (number_of_order) dari tahun 2009 sampai 2012 (years). 
 
-
+SELECT YEAR(order_date) AS years,
+ SUM(sales) AS sales, 
+ COUNT(order_status) AS number_of_order
+FROM dqlab_sales_store
+WHERE order_status = "order finished"
+GROUP BY years
+ORDER BY years ASC;
 
