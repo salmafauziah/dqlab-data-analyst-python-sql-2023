20f39ace48167d6637eb3a362bba55c53ff380e4
Salma Fauziah <salmafauziah481@gmail.com>
2023-04-05 13:08:04 +0700

    Create Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql

---
+++ 6 - Project Data Analysis for Retail: Sales Performance Report/Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql
@@ -0,0 +1,12 @@
+# Promotion Effectiveness and Efficiency by Years
+# Pada bagian ini kita akan melakukan analisa terhadap efektifitas dan efisiensi dari promosi yang sudah dilakukan selama ini
+
+# Efektifitas dan efisiensi dari promosi yang dilakukan akan dianalisa berdasarkan Burn Rate yaitu 
+# dengan membandigkan total value promosi yang dikeluarkan terhadap total sales yang diperoleh
+
+# DQLab berharap bahwa burn rate tetap berada diangka maskimum 4.5%
+
+# Formula untuk burn rate : (total discount / total sales) * 100
+
+# Buatkan Derived Tables untuk menghitung total sales (sales) dan total discount (promotion_value) 
+# berdasarkan tahun(years) dan formulasikan persentase burn rate nya (burn_rate_percentage).
