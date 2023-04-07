b3e1449a2ef3dce59ead31484967b601c2366174
Salma Fauziah <salmafauziah481@gmail.com>
2023-04-07 09:31:27 +0700

    Update Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql

--- 6 - Project Data Analysis for Retail: Sales Performance Report/Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql
+++ 6 - Project Data Analysis for Retail: Sales Performance Report/Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql
@@ -33,3 +33,16 @@ ORDER BY years ASC;
 # Data yang ditampilkan hanya untuk tahun 2012
 # Contoh output di atas hanya 5 baris pertama dari output yang diharapkan
 
+SELECT YEAR(order_date) as years, 
+	product_sub_category, 
+	product_category, 
+ 	SUM(sales) AS sales, 
+ 	SUM(discount_value) AS promotion_value, 
+ 	ROUND((SUM(discount_value)/SUM(sales))*100,2) AS burn_rate_percentage
+FROM dqlab_sales_store
+WHERE order_status = "Order Finished"
+ 	AND YEAR(order_date) = 2012
+group by years, 
+ 	product_sub_category, 
+ 	product_category
+ORDER BY sales DESC;
