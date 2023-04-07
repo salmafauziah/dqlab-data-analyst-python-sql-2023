40a34f3995903a94cc857da89eb1c5cf7e978321
Salma Fauziah <salmafauziah481@gmail.com>
2023-04-07 09:27:00 +0700

    Update Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql

--- 6 - Project Data Analysis for Retail: Sales Performance Report/Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql
+++ 6 - Project Data Analysis for Retail: Sales Performance Report/Part-3-DQLab-Store-Promotion-Effectiveness-and-Efficiency.sql
@@ -10,3 +10,26 @@
 
 # Buatkan Derived Tables untuk menghitung total sales (sales) dan total discount (promotion_value) 
 # berdasarkan tahun(years) dan formulasikan persentase burn rate nya (burn_rate_percentage).
+
+SELECT
+ YEAR(order_date) AS years, 
+ SUM(sales) as sales, 
+ SUM(discount_value) AS promotion_value,
+ ROUND((SUM(discount_value)/SUM(sales))*100,2) AS burn_rate_percentage
+FROM dqlab_sales_store
+WHERE order_status = "Order Finished"
+GROUP BY years
+ORDER BY years ASC;
+
+# Promotion Effectiveness and Efficiency by Product Sub Category
+# Pada bagian ini kita akan melakukan analisa terhadap efektifitas dan efisiensi dari promosi yang sudah dilakukan selama ini seperti pada bagian sebelumnya. 
+
+# Akan tetapi, ada kolom yang harus ditambahkan, yaitu : product_sub_category dan product_category
+
+# Adapun output yang seharusnya dihasilkan adalah sebagai berikut:
+
+# Notes :
+
+# Data yang ditampilkan hanya untuk tahun 2012
+# Contoh output di atas hanya 5 baris pertama dari output yang diharapkan
+
